4ef754ff895a2166fdca2ce85afde00034412535
Markus Klink <justjoheinz@gmail.com>
2025-01-23 14:01:42 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,15 @@
+# justjoheinz/mandelbrot
+
+Calculate the Mandelbrot set with MoonBit Webassembly.
+
+Either open the project with Live Preview in VS Code or 
+
+```sh 
+moon install
+moon build 
+python -m http.server 3000 
+# new terminal, open browser
+open http://localhost:3000/
+```
+
+![Screenshot](./screenshot.png))
---
+++ component.mjs
@@ -0,0 +1,44 @@
+//  Copyright 2024 International Digital Economy Academy
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//  http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+// @ts-check
+import importObject from './.mooncakes/peter-jerry-ye/canvas/import.mjs';
+
+class MoonBitCanvas extends HTMLCanvasElement {
+    constructor() {
+        super();
+        const wasm_url = this.attributes.getNamedItem("src")?.value
+        this.tabIndex = -1; // make it focusable
+        if (wasm_url) {
+            const context = this.getContext("2d")
+            WebAssembly.instantiateStreaming(fetch(wasm_url), importObject).then(
+                (obj) => {
+                    globalThis["peter-jerry-ye:canvas"] = {
+                        eventTarget: this,
+                        memory: obj.instance.exports["memory"]
+                    };
+                    this.onclick = (event) => {
+                        // @ts-ignore
+                        obj.instance.exports.click(context, event, new Date().getTime());
+                    }
+
+                    // @ts-ignore
+                    obj.instance.exports.draw(context, new Date().getTime());
+               }
+            )
+        }
+    }
+}
+
+customElements.define("moonbit-canvas", MoonBitCanvas, { extends: "canvas" })
